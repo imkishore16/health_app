066b9b4e806d234bca515f4c3350f204818ce43a
imkishore16 <86948685+imkishore16@users.noreply.github.com>
2021-09-11 18:22:34 +0530

    added table

--- health_app/lib/home.dart
+++ health_app/lib/home.dart
@@ -15,29 +15,34 @@ class MainScreenState extends State<MainScreen>{
     return Scaffold(
       resizeToAvoidBottomInset:false,
       appBar:AppBar(
-        title:Text('Health App'),
+        title:const Text('Health App'),
       ),
       body:Container(
         child:ListView(
           children: <Widget>[
             ListTile(
-              leading:Icon(Icons.health_and_safety_rounded, color:Colors.green),
-              title:Text(
+              leading:const Icon(Icons.health_and_safety_rounded, color:Colors.green),
+              title:const Text(
                 'BMI CALCULATOR',
                 style:TextStyle(fontSize:20,fontStyle: FontStyle.italic)
               ),
-              trailing:Icon(Icons.keyboard_arrow_right),
+              subtitle: const Text('Body Mass Index'),
+              trailing:const Icon(Icons.keyboard_arrow_right),
               onTap:(){
-                  Navigator.push(context, MaterialPageRoute(builder:(context)=>BMIpage()));
+                  Navigator.push(context, MaterialPageRoute(builder:(context)=>const BMIpage()));
               },
             ),
-            ListTile(
-              leading:Icon(Icons.health_and_safety_rounded, color:Colors.green),
-              title:Text(
-                'CALORIE COUNTER',
+             ListTile(
+              leading:const Icon(Icons.health_and_safety_rounded, color:Colors.green),
+              title:const Text(
+                'BMR CALCULATOR',
                 style:TextStyle(fontSize:20,fontStyle: FontStyle.italic)
               ),
-              trailing:Icon(Icons.keyboard_arrow_right),
+              subtitle: const  Text('Basal Metabolic Rate'),
+              trailing:const Icon(Icons.keyboard_arrow_right),
+              onTap:(){
+                  Navigator.push(context, MaterialPageRoute(builder:(context)=>const BMRpage()));
+              },
             )
           ],
         )
@@ -59,8 +64,9 @@ class BMIpage extends StatefulWidget {
 class _BMIpageState extends State<BMIpage> {
   
   var gender=['male' , 'female'];
-  var itemSelected ='male';
+  var itemSelected ;
   var displayBMI ='';
+  //var data=['A','B','c','D'];
   
   TextEditingController heightController=TextEditingController();
   TextEditingController weightController=TextEditingController();
@@ -76,7 +82,7 @@ class _BMIpageState extends State<BMIpage> {
         children:<Widget>[
 
           Padding(
-          padding:EdgeInsets.only(top:10.0,),
+          padding:const EdgeInsets.only(top:10.0,),
             child:Container(
               height:30.0,
               width:5.0,
@@ -84,19 +90,28 @@ class _BMIpageState extends State<BMIpage> {
             ),
           
            Padding(
-             padding:(EdgeInsets.only(left:25.0)),
+             padding:(const EdgeInsets.only(left:20.0,right:300.0)),
              child:
               DropdownButton<String>(
-                items:gender.map((String dropDownStringItem){
+                //hint:Text('gender'),
+                items:gender.map((String dropDownItem){
                   return DropdownMenuItem<String>(
-                    value:dropDownStringItem,
-                    child:Text(dropDownStringItem),
+                    
+                    value:dropDownItem,
+                    child:Text(dropDownItem),
                   );
                 }).toList(),
-                value:itemSelected,
+                
                 onChanged:( newValueSelected) {
-                  onDropDownItemSelected(newValueSelected);
+                  //onDropDownItemSelected(newValueSelected)
+                  setState(() {
+                    itemSelected=newValueSelected!;
+                  });
                 },
+               hint: //const Align(
+                 const Text("gender"),
+              //)
+                value:itemSelected,
               )),
 
            Padding(
@@ -115,7 +130,6 @@ class _BMIpageState extends State<BMIpage> {
               //}
               ))), 
 
-
               Padding(
             padding:(const EdgeInsets.only(top:5.0,bottom:5.0)),
             child: TextField(
@@ -129,36 +143,34 @@ class _BMIpageState extends State<BMIpage> {
                 hintText: 'in cm'
               ))),
 
-
-           /* Padding(
-            padding:(EdgeInsets.only(top:2.0,bottom:2.0)),
-            child: TextField(
-              controller:ageController,
-              keyboardType:TextInputType.number,
-              decoration:InputDecoration(
-                border: OutlineInputBorder(
-                borderRadius:BorderRadius.all(Radius.circular(10.0))
-              ),
-                labelText:'age',
-              ))),   */
-
-              Expanded(
+              
+              Padding(
+                  padding:EdgeInsets.only(left:90.0,right:90.0,bottom:10),
                 child:ElevatedButton(
                   child:const Text(
                     'CALCULATE',
                     textScaleFactor: 1.0,
                     style:TextStyle(color:Colors.black,fontStyle:FontStyle.italic),
                   ),
-                   onPressed:(){
-                      setState(() {
+                  style:ElevatedButton.styleFrom(
+                    primary: Colors.blueAccent,
+                    side: const BorderSide(width:1, color:Colors.black), 
+                    elevation: 5, 
+                  ),
+                   /*style:const ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                    fixedSize: MaterialStateProperty<Size>(fixedSize:)
+                    elevation:MaterialStateProperty.all<double>(5.0), 
+                    minimumSize: MaterialStateProperty.all<Size>(super(3.0, 3,0)),
+                  ),*/
+                onPressed:(){
+                setState(() {
                         displayBMI=calculateBMI();
-                        //print(calculateBMI());
                       });
                     }
-
                 )
-              ),
-              Expanded(child:Padding(
+              )   ,
+                Expanded(child:Padding(
                 padding:const EdgeInsets.all(8.0),
                 child:Text(displayBMI,
                 
@@ -166,25 +178,229 @@ class _BMIpageState extends State<BMIpage> {
                 ),
               ),),
 
-              const Expanded(
-                child: Padding(
-                padding:EdgeInsets.all(10.0),
-                child:Text('Severe  Thinness	< 16   ,   Moderate Thinness	16 - 17  ,   Mild Thinness	17 - 18.5   ,   Normal	18.5 - 25   ,   Overweight	25 - 30    ,   Obese Class I	30 - 35    ,     Obese Class II	35 - 40   ,   Obese Class III	> 40 ')
-              )
-              ),
 
+//TABLE
+               Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table( 
+                          
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3),
+            ),
+            columnWidths: const {
+                           0:  FixedColumnWidth(2),
+                          },
+            children:const [
+              TableRow(
+                children: [
+                  Text("Severe  Thinness	< 16",textScaleFactor: 1.2,textAlign:TextAlign.center),
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),
+
+               Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table(
+                  
+            // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3),
+            ),
+            columnWidths: const {
+                           0:  FixedColumnWidth(2),
+                          //1:  FixedColumnWidth(80)
+                          },
+            
+            children: const[
+              TableRow(
+                children: [
+                  Text("Moderate Thinness	16 - 17",textScaleFactor: 1.2,textAlign:TextAlign.center),
+                  
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),
+
+            Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table( columnWidths: const {
+                           0:  FixedColumnWidth(200 ),
+                          },
+            // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3.0),
+            ),
+            children: const[
+              TableRow(
+                children: [
+                  
+                  Text("Mild Thinness	17 - 18.5  ",textScaleFactor: 1.2,textAlign:TextAlign.center),
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),
+               Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table( 
+                  columnWidths: const {
+                           0:  FixedColumnWidth(80),
+                          },
+            // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3.0),
+            ),
+            children: const[
+              TableRow(
+                children: [
+                  Text(" Normal	18.5 - 25  ",textScaleFactor: 1.2,textAlign:TextAlign.center),                
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),
+
+               Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table( 
+                  columnWidths: const {
+                           0:  FixedColumnWidth(80),
+                          1:  FixedColumnWidth(80)
+                          },
+            // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3.0),
+            ),
+            children: const[
+              TableRow(
+                children: [
+                  Text(" Overweight	25 - 30  ",textScaleFactor: 1.2,textAlign:TextAlign.center),
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),
+
+               Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table( 
+                  columnWidths: const {
+                           0:  FixedColumnWidth(80),
+                          1:  FixedColumnWidth(80)
+                          },
+            // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3.0),
+            ),
+            children: const[
+              TableRow(
+                children: [
+                  Text("Obese Class I	30 - 35   ",textScaleFactor: 1.2,textAlign:TextAlign.center),
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),
+
+               Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table( 
+                  columnWidths: const {
+                           0:  FixedColumnWidth(80),
+                          1:  FixedColumnWidth(80)
+                          },
+            // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3.0),
+            ),
+            children: const[
+              TableRow(
+                children: [
+                  Text(" Obese Class II	35 - 40 ",textScaleFactor: 1.2,textAlign:TextAlign.center),
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),
               
-     
+                Expanded(
+                child:Padding(
+                padding:const EdgeInsets.only(left:15.0,right:15.0,top:5.0,bottom:5.0),
+                child: Table( columnWidths: const {
+                           0:  FixedColumnWidth(80),
+                          1:  FixedColumnWidth(80)
+                          },
+            // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border:TableBorder.all(width: 1,color: Colors.blue,
+            borderRadius:BorderRadius.circular(3.0),
+            ),
+            children:const  [
+               TableRow(
+                children: [
+                Text(" Obese Class III	> 40 ",textScaleFactor: 1.2,textAlign:TextAlign.center),
+                ]
+              ),
+            ]
+              ),
+              ),
+               ),   
+
+
+            // Expanded(
+            //   child:Padding(
+            //     padding: const EdgeInsets.all(20.0),
+            //     child:Table(
+            //       border:TableBorder.all(width: 1,color: Colors.blue,
+            //        borderRadius:BorderRadius.circular(3.0),
+            //       ),
+            //       children: [
+            //             TableRow(
+            //               children:[
+            //                 Text(getData(data)),
+            //               ]
+            //             )
+            //       ]
+            //     )
+            //     )
+            // )
+
+
         ],
       )
     );
   }
 
-void onDropDownItemSelected( newValueSelected){
+   
+  //  getData(List<String> x){
+  //    for(int i=0;i<=3;i++){
+  //   return (x[i]);
+  // }
+  //   //print(data);
+  //   }
+
+/*void onDropDownItemSelected( newValueSelected){
     setState() {
       itemSelected = newValueSelected;
     }
-  }  
+  }*/  
 
   String calculateBMI() {
     double height =double.parse(heightController.text);
@@ -197,28 +413,40 @@ void onDropDownItemSelected( newValueSelected){
       
 
   }
-
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 // BMR CALCULATOR ON-PRESSED
 class BMRpage extends StatefulWidget {
   const BMRpage({ Key? key }) : super(key: key);
   @override
-  _BMRpageState createState() => _BMRpageState();
+  BMRpageState createState() => BMRpageState();
 }
 
-class _BMRpageState extends State<BMIpage> {
+class BMRpageState extends State<BMRpage> {
   
   var gender=['male' , 'female'];
   var itemSelected ='male';
   var displayBMR ='';
-  var exercise=['Sedentary:Little or no exercise , Light :Exercise 1-3 times per week ,Moderate :4-5 times per week , Active :daily exercise or intense exercise 3-4 times a week , Very Active:intense exercise 6-7 times a week , Extra Active : very intense exercise daily or physical job  '];
+  var exercise=['Sedentary:Little or no exercise' , ' Light :Exercise 1-3 times per week ', 'Moderate :4-5 times per week ', 'Active :daily exercise or intense exercise 3-4 times a week ',' Very Active:intense exercise 6-7 times a week ',' Extra Active : very intense exercise daily or physical job  '];
   var itemSelected1 = '';
+  var itemSelected2='';
   
   TextEditingController heightController=TextEditingController();
   TextEditingController weightController=TextEditingController();
   TextEditingController ageController=TextEditingController();
+  //TextEditingController genderController=TextEditingController();
   
   @override
   Widget build(BuildContext context) {
@@ -248,8 +476,8 @@ class _BMRpageState extends State<BMIpage> {
                   );
                 }).toList(),
                 value:itemSelected1,
-                onChanged:(newValueSelected) {
-                  onDropDownItemSelected(newValueSelected);
+                onChanged:(newValueSelected1) {
+                  onDropDownItemSelected(newValueSelected1);
                 },
               )),
 
@@ -297,21 +525,23 @@ class _BMRpageState extends State<BMIpage> {
               ))),   
 
 
-              Padding(
+              Expanded(
+              child:Padding(
              padding:(const EdgeInsets.only(left:25.0)),
              child:
               DropdownButton<String>(
-                items:exercise.map((String dropDownStringItem){
+                items:exercise.map((String dropDownStringItem1){
                   return DropdownMenuItem<String>(
-                    value:dropDownStringItem,
-                    child:Text(dropDownStringItem),
+                    value:dropDownStringItem1,
+                    child:Text(dropDownStringItem1),
                   );
                 }).toList(),
-                value:itemSelected1,
-                onChanged:(newValueSelected) {
-                  onDropDownItemSelected(newValueSelected);
+                value:itemSelected2,
+                onChanged:(newValueSelected2) {
+                  itemSelected2=newValueSelected2!;
                 },
               )),
+              ),
 
 
               Expanded(
@@ -338,16 +568,19 @@ class _BMRpageState extends State<BMIpage> {
 
   void onDropDownItemSelected( newValueSelected){
     setState() {
-      itemSelected = newValueSelected;
+      itemSelected1 = newValueSelected;
     }
   }
 
   String calculateBMR() {
     double height =double.parse(heightController.text);
     double weight =double.parse(weightController.text);
-   // double age =double.parse(ageController.text);
+    double age =double.parse(ageController.text);
+    var male =5;
+    var female= -161;
+    var Gender=[male,female];
 
-    double BMR = (weight*10000)/(height*height);
+    double BMR = (10*weight)+(6.2*height)-(5*age);
     String result= "your BMR is $BMR";
     return result;
       
@@ -356,6 +589,7 @@ class _BMRpageState extends State<BMIpage> {
   }
 
 }
+
 
 
 
